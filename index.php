f2000390745e65caccea0630a67b7c5bd21d457f
Helen751 <okerekehelenugoeze@gmail.com>
2025-08-08 03:30:27 +0200

    u

--- index.php
+++ index.php
@@ -271,9 +271,9 @@ The platform has continued to expand since inception, never looking back. The fi
 					<div class="col-lg-4 col-md-12 col-sm-12">
 						<div class="service-grid-item text-center">
 							<span class="item-icon" style="background-image: url(assets/images/icons/bg-3.png);">
-								<i class='uil uil-headphones'></i>
+								<i class='uil uil-flag'></i>
 							</span>
-							<h2 class="item-title mb-30">Countries</h2>
+							<h2 class="item-title mb-30">Demographics</h2>
 							<p>
 								<div class="counter-items-list ul-li-center clearfix">
 						<ul class="clearfix">
